476dea4987372618448791bd84ea2cac9cacd1d6
bhavyabgada <bhavyabgada@gmail.com>
2021-07-22 22:56:29 +0530

    posts

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,6 @@ from .views import HomePageView, CreatePostView, Reko # new
 
 urlpatterns = [
     path('home/', HomePageView.as_view(), name='home'),
-    path('', Reko, name='reko'),
-    path('post/', CreatePostView.as_view(), name='add_post') # new
+    path('reko/', Reko, name='reko'),
+    path('', CreatePostView.as_view(), name='add_post') # new
 ]
