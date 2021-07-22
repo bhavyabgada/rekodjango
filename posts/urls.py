5ce93c4ffb611eb4849f5fd3539bd748fcd10156
bhavyabgada <bhavyabgada@gmail.com>
2021-07-23 00:14:14 +0530

    posts

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from .views import HomePageView, CreatePostView, Reko # new
 
 urlpatterns = [
-    path('home/', HomePageView.as_view(), name='home'),
+    # path('home/', HomePageView.as_view(), name='home'),
     path('reko/', Reko, name='reko'),
     path('', CreatePostView.as_view(), name='add_post') # new
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView  # new
 from django.urls import reverse_lazy  # new
@@ -26,12 +29,13 @@ def Reko(request):
     client = boto3.client('rekognition', aws_access_key_id=s.AWS_ACCESS_KEY_ID,
                           aws_secret_access_key=s.AWS_SECRET_ACCESS_KEY, region_name=s.AWS_S3_REGION_NAME)
     labels = client.detect_labels(
-        Image={'S3Object': {'Bucket': s.AWS_STORAGE_BUCKET_NAME, 'Name': 'images/'+latest_questions.title+'.jpeg'}},
-        MinConfidence=95)
+        Image={'S3Object': {'Bucket': s.AWS_STORAGE_BUCKET_NAME, 'Name': 'images/' + latest_questions.title + '.jpeg'}},
+        MinConfidence=65)
     text = client.detect_text(
         Image={'S3Object': {'Bucket': s.AWS_STORAGE_BUCKET_NAME, 'Name': 'images/' + latest_questions.title + '.jpeg'}},
         Filters={
             'WordFilter': {
-                'MinConfidence': 95
-            },})
-    return render(request, 'reko.html', {'request': latest_questions, 'labels': labels, 'text':text})
+                'MinConfidence': 65
+            }, })
+    return render(request, 'reko.html',
+                  {'request': latest_questions, 'labels': labels['Labels'], 'text': text['TextDetections']})
